ddfe95819148704f33e49b7c35f4706943c6ef37
André Ribeiro <70483448+Andremzzr@users.noreply.github.com>
2021-03-24 14:53:07 -0300

    Update description.js

--- description.js
+++ description.js
@@ -1,4 +1,4 @@
-ivar typeText = document.querySelector(".description")
+      var typeText = document.querySelector(".description")
       var textToBeTyped = "- Backend developer and cornflakes fan;"
       var index = 0, isAdding = true
 
